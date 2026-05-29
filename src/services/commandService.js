8ee291469ca79a312c45a01fd7f3332a4047d524
hej090224 <fc49854985@gmail.com>
2026-05-29 10:12:47 +0900

    refactor: 불필요한 로그 제거

--- src/services/commandService.js
+++ src/services/commandService.js
@@ -9,8 +9,6 @@ const COMMAND_PREFIX = '!\uc678\ucd9c\uc81c';
 const HELP_COMMAND = '!\uba85\ub839\uc5b4';
 const MESSAGE_ADMIN_ONLY =
   '\uad00\ub9ac\uc790 \uad8c\ud55c\uc774 \uc788\ub294 \uc0ac\uc6a9\uc790\ub9cc \uc678\ucd9c\uc81c \ubaa8\uc9d1 \uc790\ub3d9 \ubc1c\uc1a1\uc744 \uc870\ud68c\ud558\uac70\ub098 \ubcc0\uacbd\ud560 \uc218 \uc788\uc2b5\ub2c8\ub2e4.';
-const MESSAGE_MANUAL_RECRUITMENT_SUCCESS =
-  '\uc678\ucd9c\uc81c \ubaa8\uc9d1 \uc548\ub0b4 \uba54\uc2dc\uc9c0\ub97c \uc804\uc1a1\ud588\uc2b5\ub2c8\ub2e4. \uc678\ucd9c\uc81c \uc790\ub3d9 \ubc1c\uc1a1 \uc0c1\ud0dc\uac00 ON\uc73c\ub85c \ubcc0\uacbd\ub418\uc5c8\uc2b5\ub2c8\ub2e4.';
 const MESSAGE_USAGE = '\uc0ac\uc6a9\ubc95: `!\uc678\ucd9c\uc81c`, `!\uc678\ucd9c\uc81c on`, `!\uc678\ucd9c\uc81c off`, `!\uc678\ucd9c\uc81c \ubaa8\uc9d1`';
 
 function parseRecruitmentCommand(content) {
@@ -106,8 +104,7 @@ async function handleRecruitmentCommand(message) {
     try {
       const recruitmentMessage = await sendRecruitmentMessage(message.client);
       setAutoSendEnabled(true);
-      console.log(`[COMMAND] Manual recruitment message sent: ${recruitmentMessage.id}`);
-      await reply(message, MESSAGE_MANUAL_RECRUITMENT_SUCCESS);
+      console.log(`[COMMAND] \uc218\ub3d9 \uc678\ucd9c\uc81c \ubaa8\uc9d1 \uba54\uc2dc\uc9c0 \uc804\uc1a1 \uc644\ub8cc: ${recruitmentMessage.id}`);
     } catch (error) {
       console.error('[COMMAND] Failed to send manual recruitment message:', error);
       await reply(message, `\uc678\ucd9c\uc81c \ubaa8\uc9d1 \uc548\ub0b4 \uba54\uc2dc\uc9c0 \uc804\uc1a1\uc5d0 \uc2e4\ud328\ud588\uc2b5\ub2c8\ub2e4: ${error.message}`);
